042aad9ce64faed2b7bc15da030e486052efa93c
Justin Wheeler <justintymewheeler@gmail.com>
2014-08-24 20:40:03 -0700

    finding scroll variablz

--- js/services.js
+++ js/services.js
@@ -29,7 +29,7 @@ angular.module('Mega64.services', [])
     	console.log(scope);
         angular.element($window).bind("scroll", function() {
         	console.log(this);
-        	console.log(this.scrollY, this.screenY);
+        	console.log($(this));
              if (this.scrollTop + this.offsetHeight > this.scrollHeight) {
              	console.log(scope.limit);
              	scope.$apply(function(scope){scope.limit += 5});
