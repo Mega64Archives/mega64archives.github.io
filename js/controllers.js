cb6b41de0d3678a65b69169afb362316a56a5dab
Justin Wheeler <justintymewheeler@gmail.com>
2014-08-24 18:43:26 -0700

    logging

--- js/controllers.js
+++ js/controllers.js
@@ -7,6 +7,7 @@ angular.module('Mega64.controllers', [])
   	$scope.showId = $routeParams.show;
   	$scope.episodeId = $routeParams.episode;
     $scope.searchString = {name: ''};
+    console.log('test');
   	archives(function(d){
       $scope.shows = d;
   		if($scope.showId){
